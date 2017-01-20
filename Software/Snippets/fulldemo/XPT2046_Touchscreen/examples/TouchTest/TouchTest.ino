ee44675ae3f2c1a3d2435f90f2da945f451d78e5
Marc MERLIN <marc_soft@merlins.org>
2017-01-19 19:54:48 -0800

    New demo

---
+++ Software/Snippets/fulldemo/XPT2046_Touchscreen/examples/TouchTest/TouchTest.ino
@@ -0,0 +1,46 @@
+#include <XPT2046_Touchscreen.h>
+#include <SPI.h>
+
+#define CS_PIN  8
+// MOSI=11, MISO=12, SCK=13
+
+//XPT2046_Touchscreen ts(CS_PIN);
+#define TIRQ_PIN  2
+//XPT2046_Touchscreen ts(CS_PIN);  // Param 2 - NULL - No interrupts
+//XPT2046_Touchscreen ts(CS_PIN, 255);  // Param 2 - 255 - No interrupts
+XPT2046_Touchscreen ts(CS_PIN, TIRQ_PIN);  // Param 2 - Touch IRQ Pin - interrupt enabled polling
+
+void setup() {
+  Serial.begin(38400);
+  ts.begin();
+  while (!Serial && (millis() <= 1000));
+}
+
+void loopB() {
+  TS_Point p = ts.getPoint();
+  Serial.print("Pressure = ");
+  Serial.print(p.z);
+  if (ts.touched()) {
+    Serial.print(", x = ");
+    Serial.print(p.x);
+    Serial.print(", y = ");
+    Serial.print(p.y);
+  }
+  Serial.println();
+  delay(100);
+}
+
+void loop() {
+  if (ts.touched()) {
+    TS_Point p = ts.getPoint();
+    Serial.print("Pressure = ");
+    Serial.print(p.z);
+    Serial.print(", x = ");
+    Serial.print(p.x);
+    Serial.print(", y = ");
+    Serial.print(p.y);
+    delay(30);
+    Serial.println();
+  }
+}
+
