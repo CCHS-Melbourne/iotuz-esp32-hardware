360cd9a0e5ba05dec9634528a3f9f4c40ab41548
Marc MERLIN <marc_soft@merlins.org>
2017-02-18 07:04:15 -0800

    Replaced interrupt decoder with grey code which works perfectly reliably.
    No more debouncing problems, yeah!

--- Software/Snippets/EncoderTestPinIntr/EncoderTestPinIntr.ino
+++ Software/Snippets/EncoderTestPinIntr/EncoderTestPinIntr.ino
@@ -1,49 +1,32 @@
 /* License: Apache-2  */
 
-// Inspired from rafbuff
-// http://playground.arduino.cc/Main/RotaryEncoders
+// Inspired from fbonan
+// https://www.circuitsathome.com/mcu/reading-rotary-encoder-on-arduino/
 
 // This is better than EncoderTestIntr because it only interrupts on
 // pin change instead of some time interval.
 
-// FIXME: This is an example, but it does not work well in my experience, I still
-// get bounces and skips and jumps of multiple increases on one rotation -- merlin
-
 #define encoder0PinA 15
 #define encoder0PinB 36
 
+
 volatile int16_t encoder0Pos = 0;
+uint8_t old_AB = 0;
 
-static boolean rotating=false;      // debounce management
+// grey code
+// http://hades.mech.northwestern.edu/index.php/Rotary_Encoder
+// also read up on 'Understanding Quadrature Encoded Signals'
+// https://www.pjrc.com/teensy/td_libs_Encoder.html
+int8_t enc_states[] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0};
 
-// interrupt service routine vars
-boolean A_set = false;              
-boolean B_set = false;
 
 int16_t lastencoder = 0;
 
-void doEncoderA(){
-    // debounce
-    if ( rotating ) delay (1);  // wait a little until the bouncing is done
-
-    // Test transition, did things really change? 
-    if( digitalRead(encoder0PinA) != A_set ) {  // debounce once more
-	A_set = !A_set;
-	// adjust counter + if A leads B
-	if ( A_set && !B_set ) encoder0Pos -= 1;
-	rotating = false;  // no more debouncing until loop() hits again
-    }
-}
-
-// Interrupt on B changing state, same as A above
-void doEncoderB(){
-    if ( rotating ) delay (1); // delay in ISR? Ugh...
-    if( digitalRead(encoder0PinB) != B_set ) {
-	B_set = !B_set;
-	//  adjust counter - 1 if B leads A
-	if( B_set && !A_set ) encoder0Pos += 1;
-	rotating = false;
-    }
+/* returns change in encoder state (-1,0,1) */
+void read_encoder() {
+    old_AB <<= 2;
+    old_AB |= ((digitalRead(encoder0PinB))?(1<<1):0) | ((digitalRead(encoder0PinA))?(1<<0):0);
+    encoder0Pos += ( enc_states[( old_AB & 0x0f )]);
 }
 
 // the setup routine runs once when you press reset:
@@ -54,8 +37,11 @@ void setup() {
     pinMode (encoder0PinA, INPUT_PULLUP);
     pinMode (encoder0PinB, INPUT_PULLUP);
 
-    attachInterrupt(encoder0PinA, doEncoderA, CHANGE);
-    attachInterrupt(encoder0PinB, doEncoderB, CHANGE);
+    attachInterrupt(encoder0PinA, read_encoder, CHANGE);
+    attachInterrupt(encoder0PinB, read_encoder, CHANGE);
+
+    Serial.println("This driver works reliably on IoTuz, but generates 4 rotation clicks");
+    Serial.println("per click you can feel in the knob. This is due the the hardware and not a bug");
 }
 
 // the loop routine runs over and over again forever:
@@ -69,10 +55,6 @@ void loop() {
     }
 
     //Serial.println("Do other stuff in loop()");
-
-    // There is no debouncing, but delaying between state changes will stop most bounces
-    // from happening.
-    delay(1);
 }
 
 // vim:sts=4:sw=4
